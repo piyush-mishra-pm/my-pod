9314be106a1df16d94ed7ddbe6ca5a2ba64e1092
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-05-29 13:09:48 +0530

    Added Playing and pausing music functionality.

--- src/App.js
+++ src/App.js
@@ -12,11 +12,12 @@ function App() {
 
   const [songs, setSongs] = useState(data());
   const [currentSong,setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
       <div className="App">
           <h1>Music Player</h1>
           <Song currentSong={currentSong}/>
-          <Player/>
+          <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong}/>
       </div>
   );
 }
--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay,faAngleLeft,faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
-const Player = () => {
+const Player = ({currentSong, isPlaying, setIsPlaying}) => {
+
+    const audioRef = useRef(null);
+
+    const playSongHandler = () =>{
+        if(isPlaying){
+            audioRef.current.pause();            
+        }else{
+            audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    }
+
     return (
         <div className="player-container">
             <h1>Player</h1>
@@ -13,9 +25,10 @@ const Player = () => {
             </div>
             <div className="play-controls-container">
                 <FontAwesomeIcon className='skip-back' icon={faAngleLeft}/>
-                <FontAwesomeIcon className='play' icon={faPlay}/>
+                <FontAwesomeIcon onClick={playSongHandler} className='play' icon={faPlay}/>
                 <FontAwesomeIcon className='skip-forward' icon={faAngleRight}/>
             </div>
+            <audio ref={audioRef} src={currentSong.audio}/>
         </div>
     );
 };
